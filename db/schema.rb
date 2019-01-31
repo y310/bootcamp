05d6ca09667b4066930c418c94bfabd2e2c0d2ea
ryo_hasebe <wtc.r.h173@gmail.com>
2019-01-31 18:11:15 +0900

    watchingカラムを削除した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_25_102115) do
+ActiveRecord::Schema.define(version: 2019_01_31_090309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -266,7 +266,6 @@ ActiveRecord::Schema.define(version: 2019_01_25_102115) do
   create_table "watches", force: :cascade do |t|
     t.string "watchable_type"
     t.bigint "watchable_id"
-    t.boolean "watching"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
